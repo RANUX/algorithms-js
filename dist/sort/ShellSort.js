c7e611e3332c66caf4f51c4f66e545446b5145b1
Alexander Razzhivin <admin@httpbots.com>
2016-03-08 17:30:30 +0300

    Shell sort

--- dist/sort/ShellSort.js
+++ dist/sort/ShellSort.js
@@ -1,19 +1,30 @@
 /**
- * ShellSort
+ * ShellSort based on Knuth Gap Sequence
+ * ShellSort analysis with sequences you can find here http://www.cs.princeton.edu/~rs/shell/paperF.pdf
  */
 var ShellSort = (function () {
     function ShellSort() {
     }
     ShellSort.prototype.sort = function (a, compareFn) {
+        if (compareFn === void 0) { compareFn = function (a, b) { return (a > b); }; }
         var out, temp, inn;
-        for (out = 1; out < a.length; out++) {
-            temp = a[out];
-            inn = out;
-            while (inn > 0 && a[inn - 1] > temp) {
-                a[inn] = a[inn - 1];
-                --inn;
+        var h = 1;
+        var maxLen = Math.floor(a.length / 3);
+        // Knuth Interval Gap Sequence
+        while (h <= maxLen)
+            h += 3 * h + 1; // (1, 4, 13, 40, 121, ... )
+        // when h = 1 this is only insertion sort
+        while (h > 0) {
+            for (out = h; out < a.length; out++) {
+                temp = a[out];
+                inn = out;
+                while (inn > h - 1 && compareFn(a[inn - h], temp)) {
+                    a[inn] = a[inn - h];
+                    inn -= h;
+                }
+                a[inn] = temp;
             }
-            a[inn] = temp;
+            h = Math.floor((h - 1) / 3); // (..., 121, 40, 13, 4, 1 )
         }
         return a;
     };
--- sort/ShellSort.ts
+++ sort/ShellSort.ts
@@ -2,22 +2,36 @@
 import {ISort} from './ISort';
 
 /**
- * ShellSort
+ * ShellSort based on Knuth Gap Sequence
+ * ShellSort analysis with sequences you can find here http://www.cs.princeton.edu/~rs/shell/paperF.pdf
  */
 export default class ShellSort implements ISort {
     
-    sort( a : any[],  compareFn?: any ) : any[]
+    sort( a : any[],  compareFn = ( a, b ) => (a > b) ) : any[]
     {
         var out, temp, inn;
-        for ( out = 1; out < a.length; out++) {
-            temp = a[out];
-            inn = out;
-            while (inn > 0 && a[inn-1] > temp) {
-                a[inn] = a[inn-1];
-                --inn;
+        var h = 1;                  
+        var maxLen = Math.floor( a.length/3 );
+                                    // Knuth Interval Gap Sequence
+        while ( h <= maxLen )       // h = 3*h + 1 
+            h += 3*h + 1;           // (1, 4, 13, 40, 121, ... )
+                                    // when h = 1 this is only insertion sort
+        while ( h > 0 )
+        {
+            for ( out = h; out < a.length; out++) 
+            {
+                temp = a[out];
+                inn = out;
+                while (inn > h-1 && compareFn( a[inn-h], temp )) {
+                    a[inn] = a[ inn-h ];
+                    inn -= h;
+                }
+                a[inn] = temp;
             }
-            a[inn] = temp;
+            
+            h = Math.floor( (h-1) / 3 );          // (..., 121, 40, 13, 4, 1 )
         }
+        
         return a;
     }
 };
