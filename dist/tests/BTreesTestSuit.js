414c37a6eb169bd37787f61623757b20e7365d5e
Alexander Razzhivin <admin@httpbots.com>
2017-12-20 18:22:41 +0300

    rm dist

--- dist/tests/BTreesTestSuit.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var utils = require("./utils");
-var path = require("path");
-utils.runTests(path.join(__dirname, "btrees"));
-
-//# sourceMappingURL=BTreesTestSuit.js.map
--- dist/tests/HelpersTestSuit.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var utils = require("./utils");
-var path = require("path");
-utils.runTests(path.join(__dirname, "helpers"));
-
-//# sourceMappingURL=HelpersTestSuit.js.map
